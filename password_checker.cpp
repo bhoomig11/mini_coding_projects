9bef3440bfc10f08962b50bd9fe7c05b857ecd19
bhoomig11 <86061932+bhoomig11@users.noreply.github.com>
2024-01-14 16:03:43 +0530

    Update password_checker.cpp

--- password_checker.cpp
+++ password_checker.cpp
@@ -6,7 +6,7 @@ bool comparePassword(char* password, char * expected_password){
 	while(password[idx] != '\0'){
 		if(password[idx] != expected_password[idx]){
 			return false;
-	}
+		}
 	idx++;
 	}
 	return true;
@@ -15,15 +15,15 @@ bool comparePassword(char* password, char * expected_password){
 
 int main(){
 	//a pointer that points to the correct password
-	char *expected_password ="pass1010"; //stored in a read-only memory
+	char *expected_password = "pass1010"; //stored in a read-only memory
 	char password[100]; //array stored in stack (can be updated)
-	cin>>password;
+	cin >> password;
 	
 	if(comparePassword(password,expected_password)){
-		cout<<"Login Successful"<< endl;
+		cout << "Login Successful" << endl;
 	}
 	else{
-		cout<<"Try Again";
+		cout << "Try Again";
 	}
 	return 0;
 }
